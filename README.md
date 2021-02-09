eb91b169afeafffc670a0a093a938b42c32c92d5
Alex <alexandre.bourdeau@mailbox.org>
2021-02-08 20:19:18 -0500

    Architecture initiale

---
+++ README.md
@@ -0,0 +1,50 @@
+# TP 1 - Jeu de Poker (Fiche de réponses)
+
+  - Auteur : Hamel Bourdeau, Alexandre (`HAMA12128907`)
+  - Date de remise : 17/02/2021
+  - Estimation du temps de travail passé sur le projet : _xx_ heures.
+
+2021-02-08
+19:30 - ???
+
+## Réponses aux questions  
+ 
+Les tailles de réponses sont données à titre indicatif, mais il est clair qu'une question sur quinze (15) points attends plus de matière dans la réponse qu'une question sur cinq (5). Ne faites pas un roman, soyez direct et allez droit au but dans votre argumentation.
+ 
+### Question 1: Évolution du code légataire
+
+1. Créer une classe Player. Créer un attribut **nbWins**. Lors d'une victoire, 
+
+2. Créer une liste des instances de Player. Lors 
+
+### Question 2: Analyse des défauts du code légataire
+
+Réponse ici
+
+### Question 3: Justification des choix de conception
+
+Réponse ici
+
+### Question 4: Évolution du code objet
+
+Réponse ici
+
+## Auto-évaluation (optionnelle)
+
+Vous êtes libre de faire l'exercice de vous auto-évaluer en remplissant la grille d'évaluation du projet ci-dessous.
+
+| Élement         | Critère d'évaluation                       | Note  |
+| :---:           | :---                                       | :---: |
+|  _Questions_    | (#1) Évolution du code légataire           | /5    |
+|                 | (#2) Analyse des défauts du code légataire | /10   |
+|                 | (#3) Justification des choix de conception | /15   |
+|                 | (#4) Évolution du code objet               | /5    |
+|  _Modèles_      | Justesse & Pertinence de la conception     | /15   |
+|                 | Cohérence inter-modèles                    | /5    |
+|                 | Respect des principes de conception        | /15   |
+|  _Code_         | Qualité du code Java et du dépôt Git       | /10   |
+|                 | Cohérence du code avec les modèles         | /10   | 
+|                 | Qualité des tests                          | /10   | 
+| **Note Finale** | Questions + Modèles + Code                 | /100  | 
+
+_Cette auto-évaluation permet au correcteur de vous donner une rétro-action plus personnalisée en pointant les critères sur lesquels vous vous sur-évaluez et ceux sur lesquels au contraire vous vous sous-évaluez._
---
+++ src/test/java/uqam/inf5153/poker/MainTest.java
@@ -0,0 +1,124 @@
+package uqam.inf5153.poker;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class MainTest {
+
+
+    @Before public void initResult() { Main.result = null; }
+
+    // P1 Wins
+
+    @Test public void p1F_p2H() {
+        String p1 = "2D 5D QD KD 7D";
+        String p2 = "1S 4C KH TD 3S";
+        Main.main(new String[] {p1, p2});
+        assertEquals("P1", Main.result);
+    }
+
+    @Test public void p1F_p2P() {
+        String p1 = "2D 5D QD KD 7D";
+        String p2 = "1S 4C 1H TD 3S";
+        Main.main(new String[] {p1, p2});
+        assertEquals("P1", Main.result);
+    }
+
+    @Test public void p1F_p2F() {
+        String p1 = "2D 5D QD KD 7D" ;
+        String p2 = "1H 4H JH TH 3H" ;
+        Main.main(new String[] {p1, p2});
+        assertEquals("P1", Main.result);
+    }
+
+    // P2 Wins
+
+    @Test public void p2F_p1H() {
+        String p1 = "1S 4C KH TD 3S";
+        String p2 = "2D 5D QD KD 7D";
+        Main.main(new String[] {p1, p2});
+        assertEquals("P2", Main.result);
+    }
+
+    @Test public void p2F_p1P() {
+        String p1 = "1S 4C 1H TD 3S" ;
+        String p2 = "2D 5D QD KD 7D";
+        Main.main(new String[] {p1, p2});
+        assertEquals("P2", Main.result);
+    }
+
+    @Test public void p2F_p1F() {
+        String p1 = "1H 4H JH TH 3H";
+        String p2 = "2D 5D QD KD 7D";
+        Main.main(new String[] {p1, p2});
+        assertEquals("P2", Main.result);
+    }
+
+    // Tie cases
+
+    @Test public void tie_F() {
+        String p1 = "3H 8H JH KH 7H";
+        String p2 = "2D 5D QD KD 7D";
+        Main.main(new String[] {p1, p2});
+        assertEquals("TIE", Main.result);
+    }
+
+    @Test public void tie_P() {
+        String p1 = "1S 4C 1H TD 3S" ;
+        String p2 = "1C 5H 1D KS 7C";
+        Main.main(new String[] {p1, p2});
+        assertEquals("TIE", Main.result);
+    }
+
+    @Test public void tie_H() {
+        String p1 = "1S 4D JH TD 3H";
+        String p2 = "2D 5S JD 3D 7D";
+        Main.main(new String[] {p1, p2});
+        assertEquals("TIE", Main.result);
+    }
+
+    // Error cases
+
+    @Test public void tooManyCards() {
+        String p1 = "3H 8H JH KH 7H 7C";
+        String p2 = "2D 5D QD KD 7D";
+        Main.main(new String[] {p1, p2});
+        assertEquals("ERROR", Main.result);
+    }
+
+    @Test public void notEnoughCards() {
+        String p1 = "3H 8H JH KH 7H";
+        String p2 = "2D 5D QD KD";
+        Main.main(new String[] {p1, p2});
+        assertEquals("ERROR", Main.result);
+    }
+
+    @Test public void unknownValue() {
+        String p1 = "3H XH JH KH 7H";
+        String p2 = "2D 5D QD KD 7D";
+        Main.main(new String[] {p1, p2});
+        assertEquals("ERROR", Main.result);
+    }
+
+    @Test public void unknownSuit() {
+        String p1 = "3H 8H JH KH 7H";
+        String p2 = "2D 5D QX KD 7D";
+        Main.main(new String[] {p1, p2});
+        assertEquals("ERROR", Main.result);
+    }
+
+    // Extension : Detect cheating
+
+    @Ignore
+    @Test public void sameCardTwice() {
+        String p1 = "3H 8H JH KH 7H";
+        String p2 = "2D 8H QH KD 7D";
+        Main.main(new String[] {p1, p2});
+        assertEquals("ERROR", Main.result);
+    }
+
+}
